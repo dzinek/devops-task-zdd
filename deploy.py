dfaa6daa8b49403b3a63775893601633b946927c
Tomasz Nowodzinski <tomasz.nowodzinski@sadsharkdev.pl>
2019-10-25 10:51:12 +0200

    Issue #3 - Added deploy command line args

--- deploy.py
+++ deploy.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
+import argparse
 
-def main():
-    print("Hello World!")
+parser = argparse.ArgumentParser(description='Do a Zero Downtime Deployment by taking an old and a new AMIs.')
+parser.version = '1.0'
 
+parser.add_argument('-v', '-vv', '-vvv', '--verbose', action='count')
+parser.add_argument('--version', action='version')
 
-if __name__ == "__main__":
-    main()
+parser.add_argument('OLD_AMI_ID', type=str, help='Run deploy with old and new AMI_ID')
+parser.add_argument('NEW_AMI_ID', type=str, help='List AMI id')
+
+parser.print_help()
+
+args = parser.parse_args()
